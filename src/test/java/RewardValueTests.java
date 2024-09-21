8ff739e2fb987791ea60d349c00498ad99f23569
NeerAdole <adoleneer@gmail.com>
2024-09-22 02:33:42 +0530

    Revert "Reward value"

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -19,17 +20,11 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        int expectedMiles = (int) (cashValue / RewardValue.MILES_TO_CASH_RATE); // Correct conversion to miles
-        assertEquals(expectedMiles, rewardValue.getMilesValue());
+        assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        double expectedCash = milesValue * RewardValue.MILES_TO_CASH_RATE; // Correct conversion to cash
-        assertEquals(expectedCash, rewardValue.getCashValue());
+        assert false;
     }
 }
